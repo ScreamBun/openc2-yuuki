eab1367153564bc59ffe32908503b03b9f78ea8e
Mason Mirarchi <mumirarchi@praxiseng.com>
2021-03-25 10:34:06 -0400

    Removing actuator_src, client_src and associated files. Superseded by development in consumer, openc2, serialize and transport directories.

--- yuuki/actuator.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-
-from .actuator_src.consumer import ui
-
-if __name__ == '__main__':
-    ui.run()
--- yuuki/client.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-
-from .client_src import ui
-
-if __name__ == '__main__':
-    ui.run()
