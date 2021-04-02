38551c4caee017d57c6dcc8a19edcfac6ba4af38
Mason Mirarchi <mumirarchi@praxiseng.com>
2021-04-02 11:56:55 -0400

    Folded duplicate functionality into make_response_msg

--- yuuki/transport/base.py
+++ yuuki/transport/base.py
@@ -6,7 +6,12 @@ import time
 from ..openc2.oc2_types import OC2Msg, OC2Rsp, OC2RspParent, StatusCode
 
 
-class Transport():
+def nice_format(output):
+    pp = pprint.PrettyPrinter()
+    return pp.pformat(output)
+
+
+class Transport:
     """Base class for any transports implemented."""
 
     def __init__(self, transport_config):
@@ -29,76 +34,54 @@ class Transport():
 
         try:
             data_dict = self.serialization.deserialize(raw_data)
-            pp = pprint.PrettyPrinter()
-            nice = pp.pformat(data_dict)
-            logging.info('Received payload as a Python Dict:\n{}'.format(nice))
-
+            logging.info('Received payload as a Python Dict:\n{}'.format(nice_format(data_dict)))
         except Exception as e:
-            retval = self.make_response_msg()
-            retval.body.openc2.response.status = StatusCode.BAD_REQUEST
-            retval.body.openc2.response.status_text = 'Deserialization to Python Dict failed: {}'.format(e)
-
-            return self.serialization.serialize(retval.to_dict())
+            oc2_msg = self.make_response_msg(StatusCode.BAD_REQUEST,
+                                             'Deserialization to Python Dict failed: {}'.format(e), None)
+            return self.serialization.serialize(oc2_msg.to_dict())
 
         if "headers" not in data_dict.keys() or "body" not in data_dict.keys():
-            retval = self.make_response_msg()
-            retval.body.openc2.response.status = StatusCode.BAD_REQUEST
-            retval.body.openc2.response.status_text = 'Message missing headers and/or body.'
-
-            return self.serialization.serialize(retval.to_dict())
+            oc2_msg = self.make_response_msg(StatusCode.BAD_REQUEST, 'OpenC2 message missing headers and/or body.', None)
+            return self.serialization.serialize(oc2_msg.to_dict())
 
         try:
             oc2_msg_in = OC2Msg.init_from_dict(data_dict)
-
         except Exception as e:
-            retval = self.make_response_msg()
-            retval.body.openc2.response.status = StatusCode.BAD_REQUEST
-            retval.body.openc2.response.status_text = 'Conversion from Python Dict to Obj failed: {}'.format(e)
-
-            return self.serialization.serialize(retval.to_dict())
+            oc2_msg = self.make_response_msg(StatusCode.BAD_REQUEST,
+                                             'Conversion from Python Dict to Obj failed: {}'.format(e), None)
+            return self.serialization.serialize(oc2_msg.to_dict())
 
         try:
             actuator_callable = self.cmd_handler.get_actuator_callable(oc2_msg_in)
         except Exception as e:
-            retval = self.make_response_msg()
-            retval.body.openc2.response.status = StatusCode.BAD_REQUEST
-            retval.body.openc2.response.status_text = 'Message Dispatch failed: {}'.format(e)
-
-            return self.serialization.serialize(retval.to_dict())
-
+            oc2_msg = self.make_response_msg(StatusCode.BAD_REQUEST, 'Message Dispatch failed: {}'.format(e), oc2_msg_in.headers.from_)
+            return self.serialization.serialize(oc2_msg.to_dict())
 
         loop = asyncio.get_running_loop()
+
         try:
             oc2_rsp = await loop.run_in_executor(None, actuator_callable)
-
         except Exception as e:
-            retval = self.make_response_msg()
-            retval.body.openc2.response.status = StatusCode.BAD_REQUEST
-            retval.body.openc2.response.status_text = 'Actuator failed: {}'.format(e)
-
-            return self.serialization.serialize(retval.to_dict())
-
+            oc2_msg = self.make_response_msg(StatusCode.BAD_REQUEST, 'Actuator failed: {}'.format(e), oc2_msg_in.headers.from_)
+            return self.serialization.serialize(oc2_msg.to_dict())
 
         try:
-            oc2_msg_out = self.make_response_msg()
-            oc2_msg_out.headers.from_ = 'yuuki'
-            oc2_msg_out.headers.to = oc2_msg_in.headers.from_
+            oc2_msg_out = self.make_response_msg(None, None, oc2_msg_in.headers.from_)
             oc2_msg_out.body.openc2 = oc2_rsp
-            pp = pprint.PrettyPrinter()
-            nice = pp.pformat(oc2_msg_out.to_dict())
-            logging.info('Sending Response :\n{}'.format(nice))
+            logging.info('Sending Response :\n{}'.format(nice_format(oc2_msg_out.to_dict())))
             serialized = self.serialization.serialize(oc2_msg_out.to_dict())
             return serialized
         except Exception as e:
-            retval = self.make_response_msg()
-            retval.body.openc2.response.status = StatusCode.BAD_REQUEST
-            retval.body.openc2.response.status_text = 'Serialization failed: {}'.format(e)
-
-            return self.serialization.serialize(retval.to_dict())
+            oc2_msg = self.make_response_msg(StatusCode.BAD_REQUEST, 'Serialization failed: {}'.format(e), oc2_msg_in.headers.from_)
+            return self.serialization.serialize(oc2_msg.to_dict())
 
     @staticmethod
-    def make_response_msg():
-        retval = OC2Msg()
-        retval.body.openc2 = OC2RspParent()
-        retval.headers.created = int(round(time.time() * 1000))
-        return retval
+    def make_response_msg(status, status_text, to):
+        oc2_msg = OC2Msg()
+        oc2_msg.body.openc2 = OC2RspParent()
+        oc2_msg.headers.created = int(round(time.time() * 1000))
+        oc2_msg.body.openc2.response.status = status
+        oc2_msg.body.openc2.response.status_text = status_text
+        oc2_msg.headers.from_ = 'yuuki'
+        oc2_msg.headers.to = to
+        return oc2_msg
